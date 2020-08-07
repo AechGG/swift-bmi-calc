be39550a167b4779cab8034f69560eb4e2d9fb2d
AechGG <hazg@me.com>
2020-08-07 11:09:32 +0100

    feat: add new uiviewcontroller

---
+++ BMICalc/SecondViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.swift
+//  BMICalc
+//
+//  Created by Harrison Gittos on 07/08/2020.
+//  Copyright © 2020 Harrison Gittos. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
--- BMICalc/ViewController.swift
+++ BMICalc/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var heightSlider: UISlider!
     @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
